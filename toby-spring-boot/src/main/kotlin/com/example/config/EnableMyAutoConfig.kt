1fe47b591c26e1067081cf17af506b2916418da1
rhakdnj <chungjm0711@gmail.com>
2024-12-14 22:20:42 +0900

    feat: 동적인 자동 구성 정보 등록

--- toby-spring-boot/src/main/kotlin/com/example/config/EnableMyAutoConfig.kt
+++ toby-spring-boot/src/main/kotlin/com/example/config/EnableMyAutoConfig.kt
@@ -1,10 +1,8 @@
 package com.example.config
 
-import com.example.config.autoconfig.DispatcherServletConfig
-import com.example.config.autoconfig.TomcatWebServerConfig
 import org.springframework.context.annotation.Import
 
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.CLASS)
-@Import(DispatcherServletConfig::class, TomcatWebServerConfig::class)
+@Import(MyAutoConfigImportSelector::class)
 annotation class EnableMyAutoConfig
